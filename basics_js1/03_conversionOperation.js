319ee5f45d9621f9c3cdab9d77e9466a08a44a4d
dkOO7 <101042974+dkOO7@users.noreply.github.com>
2024-05-23 09:15:07 +0000

    operations

--- basics_js1/03_conversionOperation.js
+++ basics_js1/03_conversionOperation.js
@@ -28,4 +28,45 @@ let booleanisLoggedIn =Boolean(isLoggedIn)
 let somenumber =33
 
 let stringNumber = String(somenumber)
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+// **********************Operations********************
+
+let value = 3 
+let negvalue = -value
+// console.log(negvalue);
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2-2);
+// console.log(2**2);
+// console.log(2**3);
+// console.log(8%2);
+// console.log(6/3);
+
+let str1 ="hello"
+let str2 =" Dikshay"
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+
+// console.log(+true);
+// console.log(true+);   // error
+// console.log(+"");   // 0
+
+
+let num1,num2,num3
+
+// num1=num2=num3 = 2+2
+// console.log(num3);
+
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
