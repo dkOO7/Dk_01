f2bcbfca26c503cdedb2fe548d73f7649ffb5c6e
dkOO7 <101042974+dkOO7@users.noreply.github.com>
2024-05-23 08:38:55 +0000

    conversion of data types

---
+++ basics_js1/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "Dikshay"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 
+// null => 0
+// undefinde =NaN
+
+let isLoggedIn = 1
+
+let booleanisLoggedIn =Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);
+
+// 1 =>true , 0 => false
+// "" => false
+// " "=> true 
+// "Dikshay" => true  
+// null , undefined => false  
+
+
+let somenumber =33
+
+let stringNumber = String(somenumber)
+console.log(typeof stringNumber);
