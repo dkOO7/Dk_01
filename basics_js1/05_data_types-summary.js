f5c3edfc2885b87da309bb64c2adc4da25c251dc
dkOO7 <101042974+dkOO7@users.noreply.github.com>
2024-05-27 17:42:28 +0000

    update_memoryType

--- basics_js1/05_data_types-summary.js
+++ basics_js1/05_data_types-summary.js
@@ -11,12 +11,13 @@ let userEmail = undefined; // let userEmail;
 
 const id =Symbol('123')
 const anotherID = Symbol('123')
-console.log(id==anotherID);
+// console.log(id==anotherID);
 
 const bigNUmber = 29292921091923922n   // bigint
-console.log(typeof bigNUmber);
+// console.log(typeof bigNUmber);
 
 // Reference type (Non Primitive)
+
 // Arrays, Objects, Functions
 
 const heros = ["Ironman","Shaktiman","Batman","Hulk"]
@@ -26,13 +27,13 @@ let myObj = {
 }
 
 const myFunction = function() {
-    console.log("Hello world");
+    // console.log("Hello world");
     
 }
 
-console.log(typeof myFunction);
-console.log(typeof heros);
-console.log(typeof myObj);
+// console.log(typeof myFunction);
+// console.log(typeof heros);
+// console.log(typeof myObj);
 
 
 // ***********************Return-Values**********************
@@ -50,3 +51,27 @@ console.log(typeof myObj);
 //        Arrays  =>  object
 //        Function  =>  function
 //        Object  =>  object
+
+
+
+// ++++++++++++++++++Memory++++++++++++++++++++++
+
+// Stack (Primitive) and Heap (Non Primitive)
+
+ let myname = "Dikshay"
+ let anotherName =myname
+ anotherName = "abc"
+
+ console.log(myname);
+ console.log(anotherName);
+
+ let user1 = {
+    email: "usr123@gmail.com",
+    upi: "user@sbi"
+ }
+
+ let user2 = user1
+ console.log(user2);
+
+ user2.email = "user2@gmail.com"
+ console.log(user1);
